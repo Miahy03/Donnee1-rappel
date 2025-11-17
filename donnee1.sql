a57161e5b70ec16ee9dd80b27583858433602655
Miahy03 <hei.miahy.2@gmail.com>
2025-11-17 19:50:42 +0100

    feat: afficher le nombre d’employés en congé aujourd'hui et détails des employés en congé avec leur équipe

--- donnee1.sql
+++ donnee1.sql
@@ -30,6 +30,26 @@ LEFT JOIN Equipe T
 
 -- 4. Affichez par le nombre d’employés par contract_type, vous devez afficher le type de contrat, et le nombre d’employés associés.
 
-SELECT contract_type, COUNT(*)
-FROM Employee
-GROUP BY contract_type;
+SELECT E.type_contrat, COUNT(*)
+FROM Employe E
+GROUP BY E.type_contrat;
+
+-- 5. Afficher le nombre d’employés en congé aujourd'hui. La période de congé s'étend de start_date inclus jusqu’à end_date inclus.
+
+SELECT COUNT(DISTINCT C.employe_id)
+FROM Conge C
+WHERE CURRENT_DATE BETWEEN C.date_debut AND C.date_fin;
+
+
+-- 6. Afficher l’id, le nom, le prénom de tous les employés + le nom de leur équipe qui sont en congé aujourd’hui. Pour rappel, la end_date est incluse dans le congé, l’employé ne revient que le lendemain. 
+
+SELECT E.id,
+       E.prenom,
+       E.nom,
+       T.nom AS nom_equipe
+FROM Employe E
+LEFT JOIN Equipe T
+    ON E.equipe_id = T.id
+JOIN Conge C
+    ON C.employe_id = E.id
+WHERE CURRENT_DATE BETWEEN C.date_debut AND C.date_fin;
